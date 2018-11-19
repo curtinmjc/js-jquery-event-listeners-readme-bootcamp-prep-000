b575c3bfe76997a6e8aeef5af1fefddc52bbb92d
Mike Curtin <curtinmjc@gmail.com>
2018-11-19 12:03:50 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,21 @@
 //define functions here
+function getIt() {
+  alert('Hey!');
+}
+
+function frameIt() {
+  $('img').addClass('tasty');
+}
 
 $(document).ready(function(){
 
 // call functions here
+$('p').on('click', function() {
+  getIt();
+});
+
+$('img').on('load', function() {
+  frameIt();
+});
 
 });
